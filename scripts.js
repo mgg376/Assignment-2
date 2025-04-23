6afbaf3b7384f12acbaa08a5680db41532d3aa81
mgg376 <mgg376@nyu.edu>
2025-04-23 17:31:53 -0400

    trying to fix image issue

--- scripts.js
+++ scripts.js
@@ -1,65 +1,58 @@
 function injectImage() {
-  // Create an image element
-  const img = document.createElement("img")
 
-  // Set the image source and alt text
-  img.src =
-    "https://i.imgur.com/aDNO7UG.jpeg"
-  img.alt = "sample image"
+  const img = document.createElement("img");
+  img.src = "img.src = "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg;
+  img.alt = "sample image";  
+  img.style.width = "100%";
+  img.style.maxWidth = "600px";
 
-  // Find the target div and add the image to it
-  const container = document.getElementById("right-panel")
-  
+  const container = document.getElementById("right-panel");
   container.innerHTML = '';
-  container.appendChild(img)
+  container.appendChild(img);
 }
 
 function injectImage2() {
-  // Create an image element
-  const img = document.createElement("img")
+  const img = document.createElement("img");
+  img.src = "https://i.imgur.com/ect5aVG.jpg";
+  img.alt = "sample image";
+  img.style.width = "100%";
+  img.style.maxWidth = "600px";
 
-  // Set the image source and alt text
-  img.src =
-    "https://i.imgur.com/ect5aVG.jpeg"
-  img.alt = "Sample Image"
-
-  // Find the target div and add the image to it
-  const container = document.getElementById("right-panel")
-  
+  const container = document.getElementById("right-panel");
   container.innerHTML = '';
-  container.appendChild(img)
+  container.appendChild(img);
 }
 
 function injectImage3() {
-console.log('2')
-  // Create an image element
-  const img3 = document.createElement("img")
+  const img = document.createElement("img");
+  img.src = "https://i.imgur.com/BMJb7AW.jpg";
+  img.alt = "sample image";
+  img.style.width = "100%";
+  img.style.maxWidth = "600px";
 
-  // Set the image source and alt text
-  img3.src =
-    "https://i.imgur.com/BMJb7AW.jpeg"
-  img3.alt = "Sample Image"
-
-  // Find the target div and add the image to it
-  const container = document.getElementById("right-panel")
-  
+  const container = document.getElementById("right-panel");
   container.innerHTML = '';
-  container.appendChild(img3)
+  container.appendChild(img);
 }
 
 function injectImage4() {
-console.log('2')
-  // Create an image element
-  const img4 = document.createElement("img")
+  const img = document.createElement("img");
+  img.src = "https://i.imgur.com/UXBls0b.jpg";
+  img.alt = "sample image";
+  img.style.width = "100%";
+  img.style.maxWidth = "600px";
 
-  // Set the image source and alt text
-  img4.src =
-    "https://i.imgur.com/UXBls0b.jpeg"
-  img4.alt = "Sample Image"
-
-  // Find the target div and add the image to it
-  const container = document.getElementById("right-panel")
-  
+  const container = document.getElementById("right-panel");
   container.innerHTML = '';
-  container.appendChild(img4)
+  container.appendChild(img);
+}
+
+function resizeImage() {
+  let images = document.querySelectorAll("img");
+  images.forEach(img => {
+    if (img.alt === "sample image") {
+      img.style.width = "200px";
+      img.style.height = "auto";
+    }
+  });
 }
